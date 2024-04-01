407c9a6cf457b7fa4bd77cdfc5707f7b28034a19
alvarohc777 <aherrada@uninorte.edu.co>
2024-04-01 00:16:58 -0500

    update about us banner and link

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -1,12 +1,32 @@
 <template>
   <q-card class="my-card">
-    <div class="text-h2 text-white q-pa-xl">About Us</div>
+    <q-btn
+      dense
+      class="full-width full-height"
+      @click="navigateTo"
+      style="text-decoration: none"
+    >
+      <q-card-section class="absolute-center">
+        <div class="text-h5 text-tertiary">Join the Crew</div>
+      </q-card-section>
+    </q-btn>
   </q-card>
 </template>
+<script setup>
+const navigateTo = () => {
+  window.open(
+    'https://api.whatsapp.com/send?phone=573207349966&text=KING%20OF%20PETS',
+    '_blank'
+  )
+  // window.location.href = 'http://google.com'
+}
+</script>
 <style scoped>
 .my-card {
-  width: 60%;
   height: 70%;
-  background-color: rgba(0, 0, 0, 0.4);
+  max-width: 700px;
+  max-height: 700px;
+  aspect-ratio: 0.5;
+  background-color: rgba(0, 0, 0, 0.65);
 }
 </style>
