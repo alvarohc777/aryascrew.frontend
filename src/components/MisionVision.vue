98963702f89ad79b693edf7b0ce8dd09713d32ff
alvarohc777 <aherrada@uninorte.edu.co>
2024-05-25 00:52:29 -0500

    add join the crew section with members information

---
+++ src/components/MisionVision.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-card class="my-card shadow-8">
+    <q-btn
+      dense
+      class="full-width full-height"
+      style="text-decoration: none"
+      href="https://api.whatsapp.com/send?phone=573207349966&text=KING%20OF%20PETS"
+    >
+      <q-card-section class="absolute-center">
+        <div class="text-h5 text-tertiary">Join the Crew</div>
+      </q-card-section>
+    </q-btn>
+  </q-card>
+</template>
+
+<style scoped>
+.my-card {
+  height: 70%;
+  max-width: 700px;
+  max-height: 700px;
+  aspect-ratio: 0.5;
+  background-color: rgba(0, 0, 0, 0.65);
+}
+</style>
--- src/pages/AboutUs.vue
+++ src/pages/AboutUs.vue
@@ -1,35 +1,115 @@
 <template>
   <q-page class="flex column flex-center justify-around">
-    <div
+    <!-- <div
+      id="about-us"
       class="section-container about-us background row flex-center justify-center"
     >
-      <h1>About Us</h1>
-    </div>
+      <mision-vision />
+    </div> -->
+
     <div
       id="arya"
       class="section-container member background column flex-center justify-center"
     >
-      <div class="text-h4">Conoce a Arya</div>
-      <div class="text-subtitle2">Únete a su Crew</div>
-      <div class="text-body-1">(Sección sobre arya)</div>
+      <div class="q-pa-md row items-start q-gutter-md">
+        <q-card class="my-card" flat bordered>
+          <q-img src="src/assets/about-us/aryaScooter.png" height="600px" />
+
+          <q-card-section>
+            <div class="text-h4 q-mt-xs q-mb-xs text-orange-9">
+              <span class="text-h5 text-black">Conoce a</span> Arya
+            </div>
+            <div class="text-body1 text-grey">
+              Arya, una Border Collie, es la inspiración detrás de Arya's Crew.
+              Con su energía y liderazgo natural, guía y une a su manada,
+              encarnando el espíritu y la misión del grupo.
+            </div>
+          </q-card-section>
+
+          <q-card-actions>
+            <q-btn
+              flat
+              color="secondary"
+              icon="fa-brands fa-instagram"
+              href="https://www.instagram.com/arya_thecollieramirez/"
+            />
+          </q-card-actions>
+        </q-card>
+      </div>
     </div>
     <div
       id="alex"
       class="section-container member background column flex-center justify-center"
     >
-      <div class="text-h4">Conoce a Alex</div>
-      <div class="text-subtitle2">Ceo de Aryas Crew</div>
-      <div class="text-body-1">(Sección sobre Alex)</div>
+      <div class="q-pa-md row items-start q-gutter-md">
+        <q-card class="my-card" flat bordered>
+          <q-img src="src/assets/about-us/Alex.png" height="600px" />
+
+          <q-card-section>
+            <div class="text-h4 q-mt-xs q-mb-xs text-orange-9">
+              <span class="text-h5 text-black">Conoce a</span> Alex
+            </div>
+            <div class="text-body1 text-grey">
+              Alex, fundadora de Arya's Crew y madre de Arya, es una entrenadora
+              apasionada que lidera con ejemplo, fomentando un ambiente de
+              aprendizaje y respeto mutuo.
+            </div>
+          </q-card-section>
+
+          <q-card-actions>
+            <q-btn
+              flat
+              color="secondary"
+              icon="fa-brands fa-instagram"
+              href="https://www.instagram.com/alexandraramirezro/"
+            />
+          </q-card-actions>
+        </q-card>
+      </div>
     </div>
+
     <div
       id="kris"
       class="section-container member background column flex-center justify-center"
     >
-      <div class="text-h4">Conoce a Kris</div>
-      <div class="text-subtitle2">Info sobre kris</div>
-      <div class="text-body-1">(Sección sobre kris)</div>
-    </div>
+      <div class="q-pa-md row items-start q-gutter-md">
+        <q-card class="my-card" flat bordered>
+          <q-img src="src/assets/about-us/kris2.png" height="600px" />
 
+          <q-card-section>
+            <div class="text-h4 q-mt-xs q-mb-xs text-orange-9">
+              <span class="text-h5 text-black">Conoce a</span> Kris
+            </div>
+            <div class="text-body1 text-grey">
+              Kris, paseadora y miembro vital de Arya's Crew, destaca por su
+              pasión por los animales y su compromiso con el bienestar de cada
+              mascota que acompaña.
+            </div>
+          </q-card-section>
+
+          <q-card-actions>
+            <q-btn
+              flat
+              color="secondary"
+              icon="fa-brands fa-instagram"
+              href="https://www.instagram.com/krisbelrua/"
+            />
+          </q-card-actions>
+        </q-card>
+      </div>
+    </div>
+    <div
+      id="instagram"
+      class="section-container instagram background row flex-center justify-center"
+    >
+      <instagram-script />
+    </div>
+    <div
+      id="about-us"
+      class="section-container about-us background row flex-center justify-center"
+    >
+      <mision-vision />
+    </div>
     <div class="section-container contant-us column flex-center">
       <contact-us />
     </div>
@@ -40,18 +120,34 @@
 // import AboutUs from 'src/components/AboutUs.vue'
 
 import ContactUs from 'src/components/ContactUs.vue'
+import InstagramScript from 'src/components/InstagramScript.vue'
+import MisionVision from 'src/components/MisionVision.vue'
 </script>
+<!-- <script async src="//www.instagram.com/embed.js"></script> -->
+<!-- <script async src="https://www.tiktok.com/embed.js"></script> -->
+
 <style lang="scss" scoped>
 .section-container {
   &.about-us {
     &.background {
-      // background-image: url(src/assets/about-us/aryasAboutUs.png);
-      // background-size: cover;
-      // background-position: center;
+      background-image: url(src/assets/about-us/scooter.png);
+      background-size: cover;
+      background-position: center;
       background-color: $tertiary;
       height: 95vh;
       width: 100%;
       max-width: 1300px;
+    }
+  }
+  &.instagram {
+    &.background {
+      height: 70vh;
+      width: 100%;
+      max-width: 1300px;
+      height: 140vh;
+
+      // background: $primary;
+      background-image: linear-gradient(to bottom, $tertiary, $primary);
     }
   }
   &.member {
@@ -60,6 +156,42 @@ import ContactUs from 'src/components/ContactUs.vue'
       height: 95vh;
       width: 100%;
       max-width: 1300px;
+    }
+    &#arya {
+      background-image: linear-gradient(
+        to bottom,
+        rgb(156, 135, 103),
+        $tertiary
+      );
+      background-size: cover;
+      background-position: center;
+      > div {
+        max-width: 700px;
+      }
+    }
+    &#alex {
+      background-image: linear-gradient(
+        to bottom,
+        $tertiary,
+        rgb(156, 135, 103)
+      );
+      background-size: cover;
+      background-position: center;
+      > div {
+        max-width: 700px;
+      }
+    }
+    &#kris {
+      background-image: linear-gradient(
+        to bottom,
+        rgb(156, 135, 103),
+        $tertiary
+      );
+      background-size: cover;
+      background-position: center;
+      > div {
+        max-width: 700px;
+      }
     }
   }
   &.contant-us {
